5cf1582715d6cd3b911790be57aa42552d13ccd7
oleksii.an <oleksii.a@gocheetah.com>
2026-07-30 11:11:33 +0300

    [tsutsyk-api]: added secure localhost

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'https://localhost:3000',
       'https://tsutsyk-client--tsutsyk-live.europe-west4.hosted.app',
       'https://tsutsyk.live',
     ],
